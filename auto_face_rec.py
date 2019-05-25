fa18cbda32b8abb2ccf408a179bcd669f1123b49
DeadFishSec <37667454+deadfishhackers@users.noreply.github.com>
2019-05-25 20:43:08 +0500

    Add files via upload

---
+++ auto_face_rec.py
@@ -0,0 +1,25 @@
+import os
+import time
+os.system('clear')
+print('                  Before starting change your directory to home')
+time.sleep(5)
+os.system('clear')
+print('                  Automated Facial Recognition - starting up')
+time.sleep(5)
+os.system('clear')
+print('                                  Running')
+time.sleep(2)
+print("==============================================================")
+os.system("                face_recognition ./known/ ./unknown/ | cut -d ',' -f2 ")
+print("==============================================================")
+print('                             Target(s) found ')
+
+
+print('               __                       _____                                                            ')
+print('_____   __ ___/  |_  ____             _/ ____\____    ____  ____     _______   ____   ____  ____   ____  ')
+print('\__  \ |  |  \   __\/  _ \    ______  \   __\\__  \ _/ ___\/ __ \    \_  __ \_/ __ \_/ ___\/  _ \ / ___\ ')
+print(' / __ \|  |  /|  | (  <_> )  /_____/   |  |   / __ \\  \__\  ___/     |  | \/\  ___/\  \__(  <_> ) /_/  >')
+print(' ____  /____/ |__|  \____/             |__|  (____  /\___  >___  >____|__|    \___  >\___  >____/\___  / ')
+print('     \/                                           \/     \/    \/_____/           \/     \/     /_____/  ')
+
+
