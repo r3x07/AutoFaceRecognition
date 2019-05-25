720e5199616972dabc68fda3fca6ce86cab3c4d3
DeadFishSec <37667454+deadfishhackers@users.noreply.github.com>
2019-05-25 20:41:15 +0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+pip3 install face_recognition
+mkdir /root/known
+mkdir /root/unknown
+echo =========================================================================================================
+echo                Done! just put your targets photos in unknown and photos of people you know in known!
