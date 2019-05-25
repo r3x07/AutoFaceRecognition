a723dd2e2374046842beb416feccfedd6cc6f25e
DeadFishSec <37667454+deadfishhackers@users.noreply.github.com>
2019-05-25 08:43:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 pip3 install face_recognition
 cp auto_face_rec /usr/bin/
+cp auto_face_rec.py /root/
 mkdir /root/known
 mkdir /root/unknown
 echo =========================================================================================================
